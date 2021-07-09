e14cf198c6ff8e3fb4b9c5b939971b9450fcf544
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-09 11:24:39 +0100

    Restructure workspace

--- Language basics/User Defined Types/classes/Person.cpp
+++
@@ -1,6 +0,0 @@
-#include "Person.hpp"
-
-std::string Person::getName()
-{
-    return firstName + " " + lastName;
-}
--- Language basics/User Defined Types/constructors/testPerson.cpp
+++
@@ -1,8 +0,0 @@
-#include "Person.hpp"
-
-int main(int argc, char *argv[])
-{
-    Person p1 = Person("Mohammed", "Belgoumri", 21);
-    std::string name = p1.getName();
-    return EXIT_SUCCESS;
-}
