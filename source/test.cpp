13a5c889c403b168463e09fb1fae933457f6c29a
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-10 16:16:33 +0100

    Removed unnecessairy math.h include

--- source/test.cpp
+++ source/test.cpp
@@ -6,14 +6,7 @@ using std::string;
 
 int main(int argc, char *argv[])
 {
-    int x = 99;
-    while (x > 0)
-    {
-        cout << "Enter a number, 0 or negative to exit" << endl;
-        cin >> x;
-        string sign = x > 0? "positive" : "0 or negative";
-        cout << "Your number is " << sign << endl; 
-    }
+
     return EXIT_SUCCESS;
 }
 
