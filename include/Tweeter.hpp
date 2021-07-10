c1edcf07d0b1c621b485c6841e8ab7f256cea485
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-10 14:34:18 +0100

    Fixed Tweeter's constructor and run debug

--- include/Tweeter.hpp
+++ include/Tweeter.hpp
@@ -9,7 +9,7 @@ class Tweeter: public Person
     private:
         std::string twitterHandle;
     public:
-        Tweeter(std::string first, std::string second, int arbitrary, std::string handle);
+        Tweeter(std::string first, std::string last, int arbitrary, std::string handle);
         ~Tweeter();
 };
 
--- source/test.cpp
+++ source/test.cpp
@@ -6,7 +6,10 @@ int main(int argc, char *argv[])
 {
     Person p1("Mohammed", "Belgoumri", 21); 
     {
+        std::cout << "Entering inner block" << std::endl;
         Tweeter t1("Fatima", "Belgoumri", 18, "@fatiBelgou");
+        std::cout << "Exiting inner block" << std::endl;
+        std::string name = t1.getName();
     }
     std::cout << "After innermost block" << " " << std::endl;
     std::string name = p1.getName();
