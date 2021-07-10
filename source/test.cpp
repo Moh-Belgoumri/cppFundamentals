239d1b24030ac06d823741df27679daf4de337f4
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-10 16:54:41 +0100

    Functions: Free functions demo

--- source/test.cpp
+++ source/test.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include "include/Utility.hpp"
+
 using std::cout;
 using std::endl;
 using std::cin;
@@ -6,7 +8,15 @@ using std::string;
 
 int main(int argc, char *argv[])
 {
+    int x;
+    cout << "Enter a number" << endl;
+    cin >> x;
 
+    
+    if (isPrime(x)) cout << x << " is prime" << endl;
+    else cout << x << " is not prime" << endl;
+    if (is2MorePrime(x)) cout << x << " + 2 is prime" << endl;
+    else cout << x << " +2 is not prime" << endl;
     return EXIT_SUCCESS;
 }
 
