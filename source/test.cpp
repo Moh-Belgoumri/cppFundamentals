546ac09ef70da8f282a85808ce1484633580010b
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-10 15:37:57 +0100

    Flow of control: while demo

--- source/test.cpp
+++ source/test.cpp
@@ -1,18 +1,29 @@
 #include <bits/stdc++.h>
+#include<math.h>
 using std::cout;
 using std::endl;
 using std::cin;
 
 int main(int argc, char *argv[])
 {
-    int x, y;
-    cout << "Enter two numbers" << endl;
-    cin >> x >> y;
-    cout << x << " ";
-
-    if (x > y) cout << "is lareger than ";
-    else cout << "is not lareger than ";
-    cout << y << endl;
-    if (x + y > 10) cout << "Thanks for choosing larger numbers" << endl; 
+    int x;
+    cout << "Enter a number" << endl;
+    cin >> x;
+    bool isPrime = true;
+    int factor = 2;
+    while (factor <= sqrt(x))
+        {
+            int quotient = x / factor;
+            if (factor * quotient == x)
+            {
+                cout << "Found factorization " << factor << " * " << quotient << endl;
+                isPrime = false;
+                break;
+            } 
+            factor++;
+        } 
+        cout << x;
+        if(isPrime) cout << " is a prime number" << endl;
+        else cout << " is not a prime number" << endl;
     return EXIT_SUCCESS;
 }
