14593bdac964600871176b41e8338b7c756f691f
Moh-Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-10 16:13:08 +0100

    Flow of control: immediate if demo

--- source/test.cpp
+++ source/test.cpp
@@ -3,35 +3,19 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
 
 int main(int argc, char *argv[])
 {
-    int x;
-    cout << "Enter a number, 0 or negative to exit" << endl;
-    cin >> x;
-    
+    int x = 99;
     while (x > 0)
     {
-        switch (x)
-        {
-            case 1:
-                cout << "You entered 1" << endl;
-                break;
-            case 2:
-            case 3: 
-                cout << "You entered 2 or 3" << endl;
-                break;
-            case 4:
-                cout << "You entered 4" << endl;
-            case 5:
-                cout << "You entered 5" << endl;
-                break;
-            default:
-                cout << "You entered something else" << endl;
-                break;
-        }
         cout << "Enter a number, 0 or negative to exit" << endl;
         cin >> x;
+        string sign = x > 0? "positive" : "0 or negative";
+        cout << "Your number is " << sign << endl; 
     }
     return EXIT_SUCCESS;
 }
+
+
